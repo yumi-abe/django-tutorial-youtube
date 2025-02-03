cb69d019d0624330eac8803134299ae4c6c4600f
yumi-abe <yumibuhibuhi0610@gmail.com>
2025-02-03 12:00:07 +0900

    管理側設定

--- myproject/diary/admin.py
+++ myproject/diary/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Page
 
-# Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ["id", "created_at", "updated_at"]
