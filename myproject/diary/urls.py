0b46b54a4fdf927510852a09c1c6575b7097cb35
yumi-abe <yumibuhibuhi0610@gmail.com>
2025-02-03 11:51:38 +0900

    日記登録ページ作成

--- myproject/diary/urls.py
+++ myproject/diary/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "diary"
 urlpatterns = [
-     path("", views.index, name="index")
+     path("", views.index, name="index"),
+     path("page/create/", views.page_create, name="page_create"),
 ]
--- myproject/diary/views.py
+++ myproject/diary/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import PageForm
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
@@ -9,5 +10,21 @@ class IndexView(View):
             ZoneInfo("Asia/Tokyo")
         ).strftime("%Y年%m月%d日 %H:%M:%S")
         return render(request, "diary/index.html", {"datetime_now": datetime_now})
+
+
+class PageCreateView(View):
+    def get(self, request):
+        form = PageForm()
+        return render(request, "diary/page_form.html", {"form": form})
     
+    def post(self, request):
+        form = PageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("diary:index")
+        return render(request, "diary/page_form.html", {"form": form})
+
+
 index = IndexView.as_view()
+page_create = PageCreateView.as_view()
+
