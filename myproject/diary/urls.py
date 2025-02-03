640107b095190e20ca89c16c3417ce7d2b1d20bf
yumi-abe <yumibuhibuhi0610@gmail.com>
2025-02-03 21:22:10 +0900

    日記一覧まで作成

--- myproject/diary/urls.py
+++ myproject/diary/urls.py
@@ -5,4 +5,5 @@ app_name = "diary"
 urlpatterns = [
      path("", views.index, name="index"),
      path("page/create/", views.page_create, name="page_create"),
+     path("pages/", views.page_list, name="page_list"),
 ]
--- myproject/diary/views.py
+++ myproject/diary/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import PageForm
+from .models import Page
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
@@ -24,7 +25,12 @@ class PageCreateView(View):
             return redirect("diary:index")
         return render(request, "diary/page_form.html", {"form": form})
 
+class PageListView(View):
+    def get(self, request):
+        page_list = Page.objects.order_by("-page_date")
+        return render(request, "diary/page_list.html", {"page_list": page_list})
 
 index = IndexView.as_view()
 page_create = PageCreateView.as_view()
+page_list = PageListView.as_view()
 
