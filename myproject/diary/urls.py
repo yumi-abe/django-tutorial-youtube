c774a03a8c8e1a6bbbe95b8f2b62143baf3c6cf4
yumi-abe <yumibuhibuhi0610@gmail.com>
2025-02-03 21:38:51 +0900

    詳細ページ作成

--- myproject/diary/urls.py
+++ myproject/diary/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
      path("", views.index, name="index"),
      path("page/create/", views.page_create, name="page_create"),
      path("pages/", views.page_list, name="page_list"),
+     path("page/<uuid:id>/", views.page_detail, name="page_detail")
 ]
--- myproject/diary/views.py
+++ myproject/diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .forms import PageForm
 from .models import Page
@@ -30,7 +30,14 @@ class PageListView(View):
         page_list = Page.objects.order_by("-page_date")
         return render(request, "diary/page_list.html", {"page_list": page_list})
 
+
+class PageDetailView(View):
+    def get(self, request, id):
+        page = get_object_or_404(Page, id=id)
+        return render(request, "diary/page_detail.html", {"page": page})
+
 index = IndexView.as_view()
 page_create = PageCreateView.as_view()
 page_list = PageListView.as_view()
+page_detail = PageDetailView.as_view()
 
